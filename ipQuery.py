6df7c04d31743ad361b66b4b9706499708510f1b
FashyGainz <47154227+FashyGainz@users.noreply.github.com>
2019-03-20 12:54:38 -0500

    added time.sleep() to try and except
    Needed because Shodan has a 1 req/sec throttle. time.sleep(2) forces each query to wait 2 seconds before continuing, thus negating the issue with throttling.

--- ipQuery.py
+++ ipQuery.py
@@ -1,7 +1,8 @@
 import json
 import shodan
 import argparse
-SHODAN_API_KEY = "<YOUR SHODAN API KEY HERE>"
+import time
+SHODAN_API_KEY = "ZwSefHpz3jcNbqw4R5YmOuk3GKhbjCZw"
 api = shodan.Shodan(SHODAN_API_KEY)
 
 parser = argparse.ArgumentParser(description='Get a list of IPs and return shodan info.')
@@ -9,15 +10,20 @@ parser.add_argument('--filename', '-f', default='iplist.txt')
 args=parser.parse_args()
 
 with open(args.filename, 'r') as f:
-	ips = [line.strip() for line in f]
+        ips = [line.strip() for line in f]
 
 
 awsInfo = {}
 for ip in ips:
-	try:
-		hostinfo = api.host(ip)
-		awsInfo[ip] = hostinfo
-	except shodan.APIError, e:
-		awsInfo[ip] = '{}'.format(e)
+        try:
+                print "Retrieving Info"
+                hostinfo = api.host(ip)
+                awsInfo[ip] = hostinfo
+                time.sleep(2)
+                print "Info collected"
+        except shodan.APIError, e:
+                awsInfo[ip] = '{}'.format(e)
+                time.sleep(2)
+                print "No information found"
 
 print json.dumps(awsInfo)
