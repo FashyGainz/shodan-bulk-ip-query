9203cdfc775e877f55a02631bb312bce586ec1cd
FashyGainz <47154227+FashyGainz@users.noreply.github.com>
2019-03-21 10:52:29 -0500

    fixed request cap issue
    Fixed with time.sleep() with a period of 2 seconds between each request. Also added some text so you can follow along.

--- ipQuery.py
+++ ipQuery.py
@@ -1,7 +1,8 @@
 import json
 import shodan
 import argparse
-SHODAN_API_KEY = "<YOUR SHODAN API KEY HERE>"
+import time
+SHODAN_API_KEY = "<YOUR API KEY HERE>"
 api = shodan.Shodan(SHODAN_API_KEY)
 
 parser = argparse.ArgumentParser(description='Get a list of IPs and return shodan info.')
@@ -9,15 +10,25 @@ parser.add_argument('--filename', '-f', default='iplist.txt')
 args=parser.parse_args()
 
 with open(args.filename, 'r') as f:
-	ips = [line.strip() for line in f]
+        ips = [line.strip() for line in f]
 
 
 awsInfo = {}
 for ip in ips:
-	try:
-		hostinfo = api.host(ip)
-		awsInfo[ip] = hostinfo
-	except shodan.APIError, e:
-		awsInfo[ip] = '{}'.format(e)
+        try:
+                print "Retrieving Info"
+                hostinfo = api.host(ip)
+                awsInfo[ip] = hostinfo
+                time.sleep(2)
+                print "Info collected, dumping raw output for host"
 
-print json.dumps(awsInfo)
+                #new clean print job
+                print hostinfo
+
+        except shodan.APIError, e:
+                awsInfo[ip] = '{}'.format(e)
+                time.sleep(2)
+                print "No information found, giving up on host"
+
+# Old dirty print job
+#print json.dumps(awsInfo)
